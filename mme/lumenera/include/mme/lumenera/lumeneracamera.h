efe21e56537768bdb6b9328e1d129ecd5b9d8200
Per Magnus Walmsness <permw92@gmail.com>
2022-10-02 23:04:02 +0200

    trying to capture frame, Lumenera

--- mme/lumenera/include/mme/lumenera/lumeneracamera.h
+++ mme/lumenera/include/mme/lumenera/lumeneracamera.h
@@ -25,14 +25,16 @@ namespace mme {
 		LumeneraCamera(LumeneraCamera&& other) = default;
 		LumeneraCamera& operator=(LumeneraCamera&& other) = default;
 
-
-		
+		void capture_single();
+		ImageSize image_size() const;
 
 		//void set_exposure(double ms);
 		//void set_binning();
 	private:
 		static void close_handle(void* handle);
 		using handle_cleaner_func_t = void(*)(void*);
+
+		bool write_default_camera_settings();
 
 	private:
 		std::unique_ptr<void, handle_cleaner_func_t> m_camera_handle;
