e31c8d1bb2885198e7bfc0ef9edd7ec72c5afe57
Per Magnus Walmsness <permw92@gmail.com>
2022-10-10 21:26:08 +0200

    member funcs for setting lumenera cam properties

--- mme/lumenera/include/mme/lumenera/lumeneracamera.h
+++ mme/lumenera/include/mme/lumenera/lumeneracamera.h
@@ -2,6 +2,8 @@
 #include "mme/imaging/image.h"
 #include <memory>
 #include <functional>
+#include <variant>
+
 
 struct Exposure {
 	double value;
@@ -11,11 +13,12 @@ struct Binning {
 	size_t value;
 };
 
-
 namespace mme {
 
 	class LumeneraCamera {
 	public:
+		using Property = std::variant<Exposure, ImageSize, Binning>;
+
 		LumeneraCamera(size_t camera_num = 1);
 		
 		LumeneraCamera(const LumeneraCamera& other) = delete;
@@ -28,15 +31,30 @@ namespace mme {
 		Image<float> capture_single();
 		ImageSize image_size() const;
 
+		void set_exposure(Exposure exposure);
+		void set_image_size(ImageSize size);
+		void set_binning(Binning bin);
+
+		//void set_properties(const std::vector<Property>& properties);
+
+
+	private:
+		struct Properties {
+			Exposure exposure;
+			ImageSize image_size;
+			Binning binning;
+		};
 	private:
 		static void close_handle(void* handle);
 		using handle_cleaner_func_t = void(*)(void*);
 
 		bool write_default_camera_settings();
+		bool change_properties(Properties properties);
+
 
 	private:
 		std::unique_ptr<void, handle_cleaner_func_t> m_camera_handle;
-		//void* m_camera_handle;
+		Properties m_properties;
 	};
 
 }
