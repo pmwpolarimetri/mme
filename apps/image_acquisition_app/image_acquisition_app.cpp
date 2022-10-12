b8e2b6cbdf82b1dad25cf82fc1e356cc1cf72ed3
Per Magnus Walmsness <per.m.walmsness@ntnu.no>
2022-10-12 15:07:29 +0200

    Added some more code for file-saving etc.

--- apps/image_acquisition_app/image_acquisition_app.cpp
+++ apps/image_acquisition_app/image_acquisition_app.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <format>
-//#include <Windows.h>
 #include <span>
 #include <memory>
 #include <filesystem>
@@ -9,6 +8,7 @@
 #include <chrono>
 #include <ctime>
 #include <assert.h>
+//#include <Windows.h>
 
 #include "mme/lumenera/lumeneracamera.h"
 #include "mme/imaging/image.h"
@@ -16,24 +16,32 @@
 #include "mme/fwxc/fwxc.h"
 #include "lucamapi.h"
 #include "npy.hpp"
-//#include "asio.hpp"
+#include "asio.hpp"
 
-// Let PSG_driver be 2 for transmission mode and 3 for reflection mode
-int PSG_driver = 2;
-int PSA_driver = 1;
+// Type in a foldername of the measurement:
+std::string folderdescription = "Frosted glass";
+
+// K-space or real image?
+bool kspace = false;
+
+// Transmission or reflection mode?
+bool transmission = true;
 
 //Type in the rotations of the retarders (must be of the same size)
 std::vector<double> PSG_pos{-51.7,-15.1,15.1,51.7};
 std::vector<double> PSA_pos{-51.7,-15.1,15.1,51.7};
 
-std::string make_new_directory() {
+
+std::string make_new_directory(std::string transorref, std::string kspaceorreal) {
 	std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 	char* timenow = std::ctime(&now);
 	std::string foldername(timenow);
 	foldername.erase(std::remove(foldername.begin(), foldername.end(), '\n'), foldername.cend());
 	std::replace(foldername.begin(), foldername.end(), ':', '.');
+	foldername = foldername + " " + folderdescription + ", " + kspaceorreal + ", " + transorref;
 
-	std::string path = "C:/Users/PolarimetriD4-112/dev/mme/data/" + foldername;
+	std::string path = "C:/Users/PolarimetriD4-112/dev/mme/data/" + foldername; 
+
 
 	std::filesystem::current_path("C:/Users/PolarimetriD4-112/dev/mme/data/");
 
@@ -52,19 +60,19 @@ void camera_initialize(mme::LumeneraCamera *cam) {
 	return;
 };
 
-void driver_initialize(mme::ESPDriver* driver) {
+void driver_initialize(mme::ESPDriver* driver, int PSG_driver, int PSA_driver) {
 	auto reply = driver->request("VE?\r\n");
-	std::cout << "Initialized rotation motor: " << reply << std::endl << std::endl;
+	std::cout << "Initialized rotation motors: " << reply << std::endl << std::endl;
 	driver->home(PSG_driver);
 	driver->home(PSA_driver);
 }
 
-void measure_and_save(mme::LumeneraCamera* cam, std::string path, float PSG_pos, float PSA_pos) {
+void measure_and_save(mme::LumeneraCamera* cam, std::string path, std::string PSG_pos, std::string PSA_pos, std::string wavelength) {
 	auto image = cam->capture_single();
-	std::cout << "Captured image at PSG pos " << PSG_pos << " and PSA pos " << PSA_pos << std::endl;
+	std::cout << "Captured image at wavelength " << wavelength << std::endl;
 	std::cout << "Height: " << image.size().height << ", Width: " << image.size().width << std::endl;
 
-	auto filename = std::filesystem::path(path + "/PSG" + std::to_string(PSG_pos) + "PSA" + std::to_string(PSA_pos) + ".npy");
+	auto filename = std::filesystem::path(path + "/PSG" + PSG_pos + "PSA" + PSA_pos + "Wl" + wavelength + ".npy");
 	mme::save_to_numpy(filename.string(), image);
 	return;
 };
@@ -74,28 +82,50 @@ int main()
 
 	try {
 
-		std::string path = make_new_directory();
+		int PSG_driver;
+		std::string transorref;
+		if (transmission) {
+			PSG_driver = 2;
+			transorref = "transmission mode";
+		}
+		else {
+			PSG_driver = 3;
+			transorref = "reflection mode";
+		}
+		int PSA_driver = 1;
+
+		std::string kspaceorreal;
+		if (kspace) {
+			kspaceorreal = "k-space image";
+		}
+		else {
+			kspaceorreal = "real image";
+		}
+
+		std::string path = make_new_directory(transorref,kspaceorreal);
 
 		mme::LumeneraCamera cam{};
 		camera_initialize(&cam);
 
 		mme::ESPDriver driver{ "COM1" };
-		driver_initialize(&driver);
+		driver_initialize(&driver,PSG_driver,PSA_driver);
 
 		for (int i = 0; i != PSG_pos.size(); ++i) {
 
 			driver.move_absolute(PSG_driver, PSG_pos[i]);
 			driver.move_absolute(PSA_driver, PSA_pos[i]);
+			std::cout << "Moved to positions PSG: " << PSG_pos[i] << " PSA: " << PSA_pos[i] << std::endl;
 
-			measure_and_save(&cam, path, PSG_pos[i], PSA_pos[i]);
+			// Include mono and filterwheel for each position
+			std::string wavelength = "white spectrum";
+
+			measure_and_save(&cam, path, std::to_string(PSG_pos[i]).substr(0, std::to_string(PSG_pos[i]).size() - 5), std::to_string(PSA_pos[i]).substr(0, std::to_string(PSA_pos[i]).size() - 5), wavelength);
 		}
 
 		driver.home(PSG_driver);
 		driver.home(PSA_driver);
 
 		std::cout << "\nSuccessfully acquired images and saved to file" << std::endl;
-
-		// Include mono and filterwheel
 
 		return 1;
 	}
--- data/plot.py
+++ data/plot.py
@@ -1,15 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from glob import glob
+import os
+import math
 
-im: np.ndarray = np.load("image.npy")
+path = "C:\\Users\\Vilde\\OneDrive - NTNU\\Høst 2022\\Prosjektoppgave\\Arbeid\\Data"
+foldername = "Wed Oct 12 10.48.56 2022 Frosted glass, k-space image, transmission mode"
+#foldername = "Wed Oct 12 10.50.32 2022 Frosted glass, real image, transmission mode"
 
-print(im.size)
-print(type(im))
-print(im.dtype)
+files = glob(path+os.sep+foldername+os.sep+"*.npy")
 
-print(im)
-print(im.ravel(order="K"))
-print(im.flags)
-fig, ax = plt.subplots(1,1)
-ax.imshow(im)
+fig_rows = 4
+fig_columns = math.ceil(len(files)/fig_rows)
+
+fig, axs = plt.subplots(fig_columns,fig_rows,figsize=(5*fig_columns,5*fig_rows))
+
+for i in range(len(files)):
+    file = files[i]
+    filename = file.replace(path+os.sep+foldername+os.sep,"").replace(".npy","")
+    position = filename[:filename.find("Wl")]
+    wavelength = filename[filename.find("Wl")+2:]
+    
+    im: np.ndarray = np.load(file)
+    im = im[250:420,170:343]
+    
+    if fig_rows == 1 or fig_columns == 1:
+        ax = axs[i]
+    else:
+        ax = axs[i//fig_rows,i%fig_rows]
+        
+    ax.imshow(im)
+    
+    ax.set_title(position)
+    
+    ax.set_xticks([])
+    ax.set_yticks([])
+    
+fig.suptitle(foldername)
+fig.tight_layout()
+plt.savefig(path+os.sep+foldername+os.sep+foldername+".png") 
 plt.show()
